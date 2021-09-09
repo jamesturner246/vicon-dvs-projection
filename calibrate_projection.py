I inserted what I believe is the correct Euler angles for teh combined vicon-provided pose and vicon-to-camera rotation. However, we truly should have a test for this to make sure we didn't mess it up.

calibrate_projection.py
@@ -415,13 +415,13 @@ def rotation_matrix_to_euler_angles(M):
     m[1]= np.arccos(M[2,2])
     m[2]= np.arctan(-M[2,0]/M[2,1])
     # problem: Euler angles alpha and gamma are [-pi, pi] not just [-pi/2, pi/2] as produced by arctan. Need to find out whether we have the right angle for alpha and gamma:
-    # M[0,2] == sin(m[0])*sin(m[1]) can be used to check m[0] - it needs to produce the right sign
+    # M[0,2] == sin(m[0])*sin(m[1]) can be used to check m[0] - it needs to produce the correct sign
     if M[0,2]*np.sin(m[0])*np.sin(m[1]) < 0.0:
         if m[0] > 0.0:
             m[0]= m[0]-np.pi
         else:
             m[0]= m[0]+np.pi
-    # M[2,0] == sin(m[1])*sin(m[2]) can be used to check m[2] - it needs to produce the right sign
+    # M[2,0] == sin(m[1])*sin(m[2]) can be used to check m[2] - it needs to produce the correct sign
     if M[2,0]*np.sin(m[1])*np.sin(m[2]) < 0.0:
         if m[2] > 0.0:
             m[2]= m[2]-np.pi

projection.py
@@ -500,7 +500,8 @@ def projection():
             rotation = vicon['rotation'][prop_name]
             final_vicon_data['rotation'][prop_name].append([rotation])
             for i in range(n_camera):
-                cam_rotation = rotation + dv_space_euler_angles[i]
+                #cam_rotation = rotation + dv_space_euler_angles[i]
+                cam_rotation= rotation_matrix_to_euler_angles(np.dot(euler_angles_to_rotation_matrix(rotation),euler_angles_to_rotation_matrix(dv_space_euler_angles[i])))
                 final_vicon_data[f'camera_rotation_{i}'][prop_name].append([cam_rotation])
             for marker_name in props[prop_name].keys():
                 translation = vicon['translation'][prop_name][marker_name]
@@ -538,7 +539,8 @@ def projection():
                     rotation = f(vicon['timestamp'])
                     final_vicon_data['rotation'][prop_name][-1] = rotation
                     for i in range(n_camera):
-                        cam_rotation = rotation + dv_space_euler_angles[i]
+                        #cam_rotation = rotation + dv_space_euler_angles[i]
+                        cam_rotation= rotation_matrix_to_euler_angles(np.dot(euler_angles_to_rotation_matrix(rotation),euler_angles_to_rotation_matrix(dv_space_euler_angles[i])))
                         final_vicon_data[f'camera_rotation_{i}'][prop_name][-1] = cam_rotation
                     for marker_name in props[prop_name].keys():
                         y = np.array(vicon_translation_buffer[prop_name][marker_name])